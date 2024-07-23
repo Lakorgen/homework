ec58767fa95ec563f5783e4803078f18b0d8610b
Lakorgen <160156737+Lakorgen@users.noreply.github.com>
2024-07-23 13:59:22 +0400

    add lesson2-81

---
+++ lesson2-81/main.js
@@ -0,0 +1,10 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(let i = 0; i <= arr.length; i++){
+    if(arr[i] % 2 == 0)
+    console.log(arr[i])
+}
+console.log('===============================')
+const arr2 = ['красный', "оранжевый", "желтый", "зеленый", "голубой", "синий", "фиолетовый"]
+for(let i = arr2.length - 1; i >= 0; i--){
+    console.log(arr2[i])
+}

a2a80645a81771b913c1b8f19aef69319d993156
Lakorgen <160156737+Lakorgen@users.noreply.github.com>
2024-07-31 21:55:07 +0400

    add testimonial and Pricing and blogs + adapt

--- Kronix/js/main.js
+++ Kronix/js/main.js
@@ -6,22 +6,56 @@
         const burgerIcon = e.target.closest('.burger-icon');
         const burgerNavLink = e.target.closest('.nav__link');
 
-        if(!burgerIcon && ! burgerNavLink) return
+        if (!burgerIcon && !burgerNavLink) return
 
 
-        if(!document.body.classList.contains('body--opened-menu')){
+        if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
-        } else{
+        } else {
             document.body.classList.remove('body--opened-menu')
         }
 
     }
+
+
+    // Слайдер отзывов
+    const swiper = new Swiper('.testimonial__slider', {
+        // Optional parameters
+        autoHeight: true,
+        direction: 'horizontal',
+        slidesPerView: 1,
+        centeredSlides: true,
+        effect: "fade",
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.testimonial__next',
+            prevEl: '.testimonial__prev',
+        },
+
+
+    });
+    // Слайдер тарифов
+    const swiper2 = new Swiper('.pricing__slider', {
+        // Optional parameters
+        autoHeight: true,
+        direction: 'horizontal',
+        slidesPerView: 3,
+        slidesPerView: "auto",
+        initialSlide: 1,
+        centeredSlides: !0,
+
+        breakpoints: {
+            768: {
+                centeredSlides: !1
+            }
+        }
+
+
+
+
+    });
 })()
 
 
-// const burgerIcon = document.querySelector('.burger-icon')
-// const body = document.body
-
-//     burgerIcon.addEventListener('click', () =>{
-//         body.classList.toggle('body--opened-menu')
-//     })
